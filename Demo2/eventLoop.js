750b06b1bf277438b82c8249f9a6ab101880cc89
perry.z.chen@pwc.com <chenzhipeng901105@gmail.com>
2018-02-09 17:13:51 +0800

    add node code

---
+++ Demo2/eventLoop.js
@@ -0,0 +1,12 @@
+// Import events module
+var events = require('events');
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+  console.log('connection successful.');
+
+  eventEmitter.emit('data_received');
+}
